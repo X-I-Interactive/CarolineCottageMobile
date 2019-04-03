8275662a64642ef7d5c412c790569a4130d9870a
Peter Smith <peter.smith@npeu.ox.ac.uk>
2019-04-03 22:43:30 +0400

    WIP

--- CarolineCottageMobile/App_Start/BundleConfig.cs
+++ CarolineCottageMobile/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace CarolineCottageMobile
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                       "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
